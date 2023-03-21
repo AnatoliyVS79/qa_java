feb2e3900a7bc5c2d43ca2362d8f7beb78d9596d
Anatoliy Samoylov <a9295730700@yandex.ru>
2023-03-21 12:32:41 +0300

    add jacoco report

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,2 +1,31 @@
-package com.example;public class CatTest {
+package com.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatTest {
+    Cat cat;
+
+    @Before
+    public void setUp() throws Exception {
+        Feline feline = new Feline();
+        cat = new Cat(feline);
+    }
+
+    @Test
+    public void getCatSoundTest() {
+        String expectedSound = "Мяу";
+        assertEquals("Ожидаем звук мяу", expectedSound, cat.getSound());
+
+    }
+
+    @Test
+    public void getCatFoodTest() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        assertEquals("Ожидаем перечисление списка еды кота", expectedFood, cat.getFood());
+    }
 }
